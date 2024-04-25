c7cd173f01bd78121242da0d41117782a341842c
jeremyjim9 <jeremyj@my.ccsu.edu>
2024-04-24 23:43:58 -0400

    updated ui

--- js/results.js
+++ js/results.js
@@ -76,3 +76,27 @@ $(document).ready(function() {
         console.error('No data found in Session Storage');
     }
 });
+$('#search').click(getSafetyRatings);
+    function getSafetyRatings() {
+        const makeSearch = $('#make').val();
+        const modelSearch = $('#model').val();
+        const yearSearch = $('#year').val();
+
+        console.log(makeSearch, modelSearch, yearSearch);
+        $.ajax({
+            type: "GET",
+            url: 'https://js6i22vcp4.execute-api.us-east-1.amazonaws.com/FirstStage/GETYEARMAKEMODEL',
+            async: true,
+            data: {
+                Year: yearSearch, 
+                Make: makeSearch,
+                Model: modelSearch
+            },
+            dataType: 'json',
+            success: function(data) {
+                console.log(data);
+                sessionStorage.setItem('results',JSON.stringify(data));                
+                window.location.href = 'results.html';
+            }
+        });
+    }
